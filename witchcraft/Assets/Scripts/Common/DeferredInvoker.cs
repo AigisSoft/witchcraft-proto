bb61bc4e4cd7a9404e92a66b7d7a68f0befed78f
hoshino nono <pwmt.stars.games@gmail.com>
2020-07-07 01:43:09 +0900

    DeferredInvoker追加

---
+++ witchcraft/Assets/Scripts/Common/DeferredInvoker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeferredInvoker
+{
+    List<Action> _entries = new List<Action>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void clear()
+    {
+        _entries.Clear();
+    }
+
+    // Update is called once per frame
+    public void push(Action func)
+    {
+        _entries.Add(func);
+    }
+
+    // Update is called once per frame
+    public bool resume()
+    {
+        if(_entries.Count == 0) {
+            return false;
+        }
+
+        Action func = _entries[0];
+        _entries.RemoveAt(0);
+        func();
+
+        return true;
+    }
+}
