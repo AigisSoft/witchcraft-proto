b0c521cd87df538c115e1e15dffb7f3adf2aa4e5
hirokazuma <hiro.kazu.hirokazu122@gmail.com>
2020-09-01 01:16:02 +0900

    MessageDialogを作成できるようにする

---
+++ witchcraft/Assets/Scripts/Commons/WindowFactory.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Commons
+{
+    public class WindowFactory : MonoBehaviour
+    {
+        public GameObject MessageDialogObj;
+
+
+        private List<GameObject> windowObjList = null;
+        static private WindowFactory instance = null;
+        static public WindowFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("WindowFactory");
+                    instance = obj.AddComponent<WindowFactory>();
+
+                    instance.Initialize();
+                }
+                return instance;
+            }
+        }
+
+        public void Initialize()
+        {
+            windowObjList = new List<GameObject>();
+            if (!LoadPlefab())
+                return;//Error
+
+        }
+
+        private bool LoadPlefab()
+        {
+            MessageDialogObj = (GameObject)Resources.Load("Prefabs/Window/MessageDialog");
+            if (MessageDialogObj == null)
+                return false;
+
+            return true;
+        }
+
+        public GameObject MessageDialogCreate()
+        {
+            if(MessageDialogObj == null)
+            {
+                return null;
+            }
+
+            GameObject _obj = Instantiate(MessageDialogObj);
+            if (_obj == null)
+            {
+                return null;
+            }
+
+            windowObjList.Add(_obj);
+            return _obj;
+        }
+
+
+        public void DestroyWindow(GameObject i_destroyObj)
+        {
+            int index = windowObjList.IndexOf(i_destroyObj);
+            if (index == -1)
+            {
+                return;
+            }
+
+            Destroy(windowObjList[index]);
+            windowObjList.RemoveAt(index);
+        }
+    }
+}
--- witchcraft/Assets/Scripts/Commons/WindowManager.cs
+++ witchcraft/Assets/Scripts/Commons/WindowManager.cs
@@ -1,26 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Commons
 {
-    enum E_WindowType
+    public enum WindowType
     {
-        none
+        none,
+        MessageDialog,
     }
 
-    public class WindowManager : MonoBehaviour
+    public enum WINDOW_ERROR
     {
+        NONE = 0x0000,
+
+        CREATE_ERROR = 0x0100,
+    }
+
+    public class WindowManager : Singleton<WindowManager>
+    {
+        static private List<Window> windowList = null;
+        static Canvas canvas = null;
+
+        private WindowFactory factory = null;
+
+        //初期化
+        public override void Initialize()
+        {
+            windowList = new List<Window>();
+            canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+
+            factory = WindowFactory.Instance;
+        }
+
+
         // Start is called before the first frame update
         void Start()
         {
 
         }
 
-        // Update is called once per frame
-        void Update()
+        public MessageDialog MessageDialogCreate(string i_message)
         {
+            GameObject _obj = factory.MessageDialogCreate();
+            if(_obj == null)
+            {
+               return null;
+            }
 
+            MessageDialog _md = _obj.AddComponent<MessageDialog>();
+
+            _md.Message = i_message;
+
+            if(!_md.Initialize())
+            {
+                factory.DestroyWindow(_obj);
+                return null;
+            }
+
+            windowList.Add(_md);
+            return _md;
+        }
+
+        public void DestroyWindow(Window i_window)
+        {
+            if(i_window == null)
+            {
+                return;
+            }
+
+            i_window.Close();
+            windowList.Remove(i_window);
+            factory.DestroyWindow(i_window.gameObject);
+        }
+
+        //解放処理
+        ~WindowManager()
+        {
+            if(windowList != null)
+            {
+                if (windowList.Count > 0)
+                {
+                    foreach (Window _wid in windowList)
+                    {
+                        _wid.Close();
+                    }
+                }
+            }
         }
     }
 }
