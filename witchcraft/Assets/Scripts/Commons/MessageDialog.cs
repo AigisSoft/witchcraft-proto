2b639f359fc3de9e7b1e2ef4313bf8a25c4de8a8
hirokazuma <hiro.kazu.hirokazu122@gmail.com>
2020-09-01 01:15:05 +0900

    Messageを表示するDialogクラスの作成

---
+++ witchcraft/Assets/Scripts/Commons/MessageDialog.cs
@@ -0,0 +1,42 @@
+﻿using Commons;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageDialog : Commons.Window
+{
+    private string message = string.Empty;
+    public string Message
+    {
+        get { return message; }
+        set { message = value; }
+    }
+
+    private Text messageText = null;
+
+    public override bool Initialize()
+    {
+        if(!base.Initialize())
+        {
+            return false;
+        }
+
+        messageText = this.transform.Find("Message").gameObject.GetComponent<Text>();
+        if(messageText == null)
+        {
+            return false;
+        }
+
+        UpdateMessage();
+        return true;
+    }
+    
+    /// <summary>
+    /// メッセージ更新処理
+    /// </summary>
+    public void UpdateMessage()
+    {
+        messageText.text = message;
+    }
+}
