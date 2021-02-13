e4381784379693a52340f18b2292a7dbaf24a708
hirokazuma <hiro.kazu.hirokazu122@gmail.com>
2021-02-13 16:14:40 +0900

    [Sound]HandleとManager作成

---
+++ witchcraft/Assets/Scripts/Commons/SoundHandle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assets.Scripts.Commons
+{
+    public class SoundHandle
+    {
+        UInt16 Category = 0;
+        UInt16 Number = 0;
+
+        public SoundHandle(UInt16 category = 0, UInt16 number = 0)
+        {
+            Category = category;
+            Number = number;
+        }
+    }
+}
---
+++ witchcraft/Assets/Scripts/Commons/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Commons
+{
+    public enum SoundCategory
+    {
+        NONE,
+        BGM,    //2D Loop
+        UI,     //3D
+        BATTLE, //2D
+    }
+
+
+    public class SoundManager : MonoBehaviour
+    {
+        public SoundHandle Play()
+        {
+            SoundHandle handle = new SoundHandle();
+
+
+
+            return handle;
+        }
+    }
+}
+
