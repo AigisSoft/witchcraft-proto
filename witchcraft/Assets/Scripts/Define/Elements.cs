4dcb728c2af6102b2c994f1d993c13f35e3820bf
hoshino_nono <pwmt.stars.games@gmail.com>
2021-03-20 20:16:18 +0900

    ElementsのDefine作成

---
+++ witchcraft/Assets/Scripts/Define/Elements.cs
@@ -0,0 +1,22 @@
+﻿namespace Define.Elements
+{
+    public enum TYPE
+    {
+        /// <summary>
+        /// 四元素:火
+        /// </summary>
+        PYRO,
+        /// <summary>
+        /// 四元素:風
+        /// </summary>
+        AERO,
+        /// <summary>
+        /// 四元素:水
+        /// </summary>
+        HYDRO,
+        /// <summary>
+        /// 四元素:土
+        /// </summary>
+        GEO,
+    }
+}
