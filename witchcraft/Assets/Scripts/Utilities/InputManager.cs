1c5c9607fcdb9d44ce074649a3f61030a0ad6717
shigeyama_48 <mono_48@outlook.jp>
2021-06-19 17:35:56 +0900

    inputmanager更新

--- witchcraft/Assets/Scripts/Utilities/InputManager.cs
+++ witchcraft/Assets/Scripts/Utilities/InputManager.cs
@@ -3,6 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public static class InputDefine
+{
+    // Button
+    public const string BUTTON_A = "A_Button";
+    public const string BUTTON_B = "B_Button";
+    public const string BUTTON_X = "X_Button";
+    public const string BUTTON_Y = "Y_Button";
+    public const string BUTTON_L = "L_Button";
+    public const string BUTTON_R = "R_Button";
+    public const string BUTTON_VIEW = "View_Button";
+    public const string BUTTON_MENU = "Menu_Button";
+
+    // StickInput
+    public const string STICK_L = "L_Stick";
+    public const string STICK_R = "R_Stick";
+    public const string D_PAD = "DPad";
+    // TriggerInput
+    public const string TRIGGER_L = "L_Trigger";
+    public const string TRIGGER_R = "R_Trigger";
+}
+
+
 public class InputManager : SingletonMonoBehaviour<InputManager>
 {
     Dictionary<KeyCode, List<Action>> upInputCallbackTable = new Dictionary<KeyCode, List<Action>>();
@@ -11,13 +33,16 @@ public class InputManager : SingletonMonoBehaviour<InputManager>
 
     Dictionary<KeyCode, List<Action>> holdInputCallbackTable = new Dictionary<KeyCode, List<Action>>();
 
+    Dictionary<string, List<Action<Vector2>>> stickInputCallbackTable = new Dictionary<string, List<Action<Vector2>>>();
+
+    Dictionary<string, List<Action<float>>> triggerInputCallbackTable = new Dictionary<string, List<Action<float>>>();
 
     /// <summary>
     /// アップデート
     /// </summary>
     void Update()
     {
-        foreach(var code in upInputCallbackTable)
+        foreach (var code in upInputCallbackTable)
         {
             if (Input.GetKeyUp(code.Key))
             {
@@ -47,6 +72,32 @@ public class InputManager : SingletonMonoBehaviour<InputManager>
                 {
                     code.Value[i]?.Invoke();
                 }
+            }
+        }
+
+        foreach (var code in stickInputCallbackTable)
+        {
+            string stickHorizontalAxis = code.Key + "_H";
+            string stickVerticalAxis = code.Key + "_V";
+
+            float horizontal = Input.GetAxis(stickHorizontalAxis);
+            float vertical = Input.GetAxis(stickVerticalAxis);
+
+            Vector2 vec = new Vector2(horizontal, vertical);
+
+            for (int i = 0; i < code.Value.Count; i++)
+            {
+                code.Value[i]?.Invoke(vec);
+            }
+        }
+
+        foreach (var code in triggerInputCallbackTable)
+        {
+            float triggerValue = Input.GetAxis(code.Key);
+
+            for (int i = 0; i < code.Value.Count; i++)
+            {
+                code.Value[i]?.Invoke(triggerValue);
             }
         }
     }
@@ -89,9 +140,87 @@ public class InputManager : SingletonMonoBehaviour<InputManager>
         if (!holdInputCallbackTable.ContainsKey(code))
         {
             holdInputCallbackTable.Add(code, new List<Action>());
-            return;
         }
         holdInputCallbackTable[code].Add(callback);
+    }
+
+    /// <summary>
+    /// ボタンを離したときのイベント登録
+    /// </summary>
+    /// <param name="code">キーコード</param>
+    /// <param name="callback">コールバック</param>
+    public void SetUpInputCallback(String code, Action callback)
+    {
+        SetUpInputCallback(SetKeyCode(code), callback);
+    }
+
+    /// <summary>
+    /// ボタンを押したときのイベント登録
+    /// </summary>
+    /// <param name="code">キーコード</param>
+    /// <param name="callback">コールバック</param>
+    public void SetDownInputCallback(String code, Action callback)
+    {
+        SetDownInputCallback(SetKeyCode(code), callback);
+    }
+
+    /// <summary>
+    /// ボタンを押しているときのイベント登録
+    /// </summary>
+    /// <param name="code">キーコード</param>
+    /// <param name="callback">コールバック</param>
+    public void SetHoldInputCallback(String code, Action callback)
+    {
+        SetHoldInputCallback(SetKeyCode(code), callback);
+    }
+
+
+    public void SetStickInputCallback(string code, Action<Vector2> callback)
+    {
+        if (!stickInputCallbackTable.ContainsKey(code))
+        {
+            stickInputCallbackTable.Add(code, new List<Action<Vector2>>());
+        }
+        stickInputCallbackTable[code].Add(callback);
+    }
+
+    public void SetTriggerInputCallbck(string code, Action<float> callback)
+    {
+        if (!triggerInputCallbackTable.ContainsKey(code))
+        {
+            triggerInputCallbackTable.Add(code, new List<Action<float>>());
+        }
+        triggerInputCallbackTable[code].Add(callback);
+    }
+
+    /// <summary>
+    /// 文字列で受け取った
+    /// </summary>
+    /// <param name="codeStr">key文字列</param>
+    /// <returns>keycode</returns>
+    KeyCode SetKeyCode(string codeStr)
+    {
+        switch(codeStr)
+        {
+            case InputDefine.BUTTON_A:
+                return KeyCode.JoystickButton0;
+            case InputDefine.BUTTON_B:
+                return KeyCode.JoystickButton1;
+            case InputDefine.BUTTON_X:
+                return KeyCode.JoystickButton2;
+            case InputDefine.BUTTON_Y:
+                return KeyCode.JoystickButton3;
+            case InputDefine.BUTTON_L:
+                return KeyCode.JoystickButton4;
+            case InputDefine.BUTTON_R:
+                return KeyCode.JoystickButton5;
+            case InputDefine.BUTTON_VIEW:
+                return KeyCode.JoystickButton6;
+            case InputDefine.BUTTON_MENU:
+                return KeyCode.JoystickButton7;
+            default:
+                return KeyCode.None;
+        }
     }
 
     /// <summary>
